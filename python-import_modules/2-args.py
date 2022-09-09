f10cef1a23b3dafb39c301ca5ce8f29c400a16b1
guenoel <guenooel@hotmail.com>
2022-09-09 14:29:56 +0200

    task 2 module python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,11 @@
 if __name__ == "__main__":
     import sys
     argz = sys.argv
-    if len(argz) == 2:
+    if len(argz) == 1:
+        print("0 arguments.")
+    elif len(argz) == 2:
         print("1 argument:")
     else:
         print("{} arguments:".format(len(argz)-1))
-    for i in range (1, len(argz)):
+    for i in range(1, len(argz)):
         print("{}: {}".format(i, argz[i]))
