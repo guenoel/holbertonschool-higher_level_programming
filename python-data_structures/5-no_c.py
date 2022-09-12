182010e8b004953fdda3e0c87658c78966618f11
guenoel <guenooel@hotmail.com>
2022-09-12 15:32:20 +0200

    task 5 list Python

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
+    if my_string and len(my_string) != 0:
         my_string2 = ""
         for i in range(len(my_string)):
             if my_string[i] != 'c' and my_string[i] != 'C':
