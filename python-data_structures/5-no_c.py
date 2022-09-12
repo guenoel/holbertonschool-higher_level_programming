e325efd8eb35a7abadf99732d6f636f3d7392d43
guenoel <guenooel@hotmail.com>
2022-09-12 15:49:42 +0200

    task 5 list Python

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,6 +4,4 @@ def no_c(my_string):
         for i in range(len(my_string)):
             if my_string[i] != 'c' and my_string[i] != 'C':
                 my_string2 = my_string2 + my_string[i]
-        if len(my_string) == 0:
-            my_string2 = " "
         return(my_string2)
