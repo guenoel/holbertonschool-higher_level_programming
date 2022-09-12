b43477cfb9fe4e7ea0f5a154338c1aeade9cd978
guenoel <guenooel@hotmail.com>
2022-09-12 22:23:24 +0200

    task 6 list Python

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{}".format(matrix[i][j]), end='')
+                print("{:d}".format(matrix[i][j]), end='')
                 if j != len(matrix[i])-1:
-                    print(' ', end='')
-            print()
+                    print("{}".format(' '), end='')
+            print("{}".format(''))
     else:
         print()
