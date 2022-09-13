4ceaff3dac6e74965b9a71f6fa45944e1fff6e65
guenoel <guenooel@hotmail.com>
2022-09-13 19:03:35 +0200

    task 1 datastruct Python

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     my_new_list = my_list.copy()
-    #return list(map(lambda x: x.replace(search, replace), my_new_list)) only for strings
     for i in range(len(my_new_list)):
         if my_new_list[i] == search:
             my_new_list[i] = replace
