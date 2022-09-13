624500a20e2e9a3b05db2a078b2f30e3886e89fb
guenoel <guenooel@hotmail.com>
2022-09-13 22:31:11 +0200

    task 3 datastruc Python

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    if not set_1 or not set_2:
+        return
     for element in set_1:
         if element in set_2:
             return list(element)
+        else:
+            return
