5999db35991822cc0984eea61ddfde81b7a2fde9
guenoel <guenooel@hotmail.com>
2022-09-14 18:23:33 +0200

    task 11 datastruct Python

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = []
-    if my_list:
-        new_list = [element*number for element in my_list]
-    return new_list
+    new_list = map(lambda element: element * number, my_list)
+    return list(new_list)
