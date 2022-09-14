7a04c35db2adb4c787937a2d60f7b05ca7393d45
guenoel <guenooel@hotmail.com>
2022-09-14 09:23:25 +0200

    task 3 datastruct Python

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,6 +2,8 @@
 def common_elements(set_1, set_2):
     if not set_1 or not set_2:
         return
+    common = []
     for element in set_1:
         if element in set_2:
-            return list(element)
+            common.append()
+        return common
