e743cd256353d9eae404cfeb545c21be460a37ea
guenoel <guenooel@hotmail.com>
2022-09-14 12:36:28 +0200

    task 6 datastruct Python

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return
+    for key in sorted(a_dictionary):
+        print("%s: %s" % (key, a_dictionary[key]))
