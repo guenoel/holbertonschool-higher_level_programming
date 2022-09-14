d07cde6d6ffddef499d7c06e6603e1e7568b11e2
guenoel <guenooel@hotmail.com>
2022-09-14 14:55:41 +0200

    task 7 datastruct

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary and key:
-        a_dictionary.update({key:value})
-    return a_dictionary
-
+    return a_dictionary.update({key:value})
