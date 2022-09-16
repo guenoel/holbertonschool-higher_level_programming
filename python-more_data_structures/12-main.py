69fd94a93a93c22ebbd436cf6f3bf387af7cf65a
guenoel <guenooel@hotmail.com>
2022-09-16 19:32:14 +0200

    task 12 exception Python

--- python-more_data_structures/12-main.py
+++ python-more_data_structures/12-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Roman to Integer test file
+""" Test file
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
@@ -16,4 +16,11 @@ roman_number = "LXXXVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+print(roman_to_int(89))
+
+print(roman_to_int(None))
+
+roman_number = "AZERTY"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) == str or roman_string:
-        valeurs = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        result = 0
-        tmp = valeurs[roman_string[len(roman_string) - 1]]
+    result = 0
+    valeurs = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+            }
+    roman_string = str(roman_string)
+    if set(valeurs) & set(roman_string):
+        tmp = valeurs[roman_string[-1]]
         for letter in reversed(roman_string):
             for k in valeurs:
                 if letter == k:
