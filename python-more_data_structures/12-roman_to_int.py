422dbfa1e830e855a6bc78c13b17b8dc127d1708
guenoel <guenooel@hotmail.com>
2022-09-16 19:11:06 +0200

    task 12 exception Python

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    valeurs = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    #for letter in range(0, len(roman_string)):
-    result = 0
-    tmp = valeurs[roman_string[len(roman_string) - 1]]
-    for letter in reversed(roman_string):
-        for k in valeurs:
-            if letter == k:
-                if valeurs[letter] < tmp:
-                    result -= valeurs[letter]
-                else:
-                    result += valeurs[letter]
-        tmp = valeurs[letter]
+    if type(roman_string) == str or roman_string:
+        valeurs = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        result = 0
+        tmp = valeurs[roman_string[len(roman_string) - 1]]
+        for letter in reversed(roman_string):
+            for k in valeurs:
+                if letter == k:
+                    if valeurs[letter] < tmp:
+                        result -= valeurs[letter]
+                    else:
+                        result += valeurs[letter]
+            tmp = valeurs[letter]
     return result
