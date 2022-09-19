d814223a0a4d4c03564cd21c21b3ac68a6d37558
guenoel <guenooel@hotmail.com>
2022-09-19 21:56:59 +0200

    task 3 classes Python

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,4 +14,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """calcul de l aire"""
+
         return int(self.__size) ** 2
