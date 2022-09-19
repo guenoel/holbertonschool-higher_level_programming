69e6c770c6984c328f58d27c35e532c647f3899e
guenoel <guenooel@hotmail.com>
2022-09-19 15:31:04 +0200

    task 1 classes Python

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""commentaire principal"""
+
+
+class Square:
+    """commentaire de classe"""
+
+    def __init__(self, size=None):
+        """commentaire d'instance"""
+
+        pass
