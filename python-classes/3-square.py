2ab151704924351f293eaf67be760abc65f9adf0
guenoel <guenooel@hotmail.com>
2022-09-19 21:55:05 +0200

    task 3 classes Python

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""commentaire principal"""
+
+
+class Square:
+    """commentaire de classe"""
+
+    def __init__(self, size=0):
+        """commentaire d'instance"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return int(self.__size) ** 2
