f1655b3a57f744a24530796bc9a67de4ddee1210
guenoel <guenooel@hotmail.com>
2022-09-20 16:41:11 +0200

    task 0 .py TDD Python

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module de fonctions mathématiques"""
+
+
 def add_integer(a, b=98):
+    """Function that add 2 variables"""
+
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     elif type(a) is not int and type(a) is not float:
