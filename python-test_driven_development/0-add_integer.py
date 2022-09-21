58e5722f79197b6f1fc6e2ef6082e97bb9789724
guenoel <guenooel@hotmail.com>
2022-09-21 10:37:01 +0200

    Task 0 First Try TDD Python

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     """function that add two variables"""
 
-    if 	b is None or type(b) is not int and type(b) is not float or b != b:
+    if b is None or type(b) is not int and type(b) is not float or b != b:
         raise TypeError("b must be an integer")
     elif a is None or type(a) is not int and type(a) is not float or a != a:
         raise TypeError("a must be an integer")
