e19e10b1b7a06e0d70d3c5eaac6620ac1af008e3
guenoel <guenooel@hotmail.com>
2022-09-21 18:35:23 +0200

    task 1 TDD Python

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -3,6 +3,16 @@
 
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[3, 8], [12, 11]]
-print(matrix_divided(matrix, None))
-print(matrix)
+try:
+    matrix = [[3, "9"], [12, 3]]
+    print(matrix_divided(matrix, 2))
+    print(matrix)
+except Exception as e:
+    print(e)
+
+try:
+    matrix = [[3, 9], [12, 3]]
+    print(matrix_divided(matrix, "2"))
+    print(matrix)
+except Exception as e:
+    print(e)
