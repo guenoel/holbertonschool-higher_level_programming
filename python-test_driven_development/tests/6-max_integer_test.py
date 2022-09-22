6e498ebfeb0455476c3e239af32a256902746151
guenoel <guenooel@hotmail.com>
2022-09-22 20:41:49 +0200

    Task 5 TDD Python

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,9 +4,25 @@ from configparser import MAX_INTERPOLATION_DEPTH
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    def test_classic(self):
+    def test_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_at_begining(self):
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+    def test_in_middle(self):
+        self.assertEqual(max_integer([3, 2, 4, 1]), 4)
+
+    def test_one_neg(self):
+        self.assertEqual(max_integer([3, 2, -4, 1]), 3)
+
+    def test_all_neg(self):
+        self.assertEqual(max_integer([-3, -2, -4, -1]), -1)
+
+    def test_one_elem(self):
+        self.assertEqual(max_integer([4]), 4)
 
     def test_empty(self):
         self.assertEqual(max_integer(), None)
