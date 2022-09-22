6da173f63539d01a9887ef1f0e4cfa3969becf8a
guenoel <guenooel@hotmail.com>
2022-09-22 11:50:37 +0200

    task 2 TDD Python

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Module d affichage de string"""
 
-import string
-
 def say_my_name(first_name, last_name=""):
     """fonction qui affiche le nom"""
 
-    if type(first_name) is str:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
+    if type(first_name) is not str or first_name is None:
         raise TypeError("first_name must be a string")
+    elif type(last_name) is not str or last_name is None:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
