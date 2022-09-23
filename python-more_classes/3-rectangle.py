086bc3398a6ae56b4688adac0d40255814ccdb14
guenoel <guenooel@hotmail.com>
2022-09-23 14:08:14 +0200

    task 3 more classes Python

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -54,6 +54,7 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rect += "#"
-            rect += "\n"
+            if i != self.__height-1:
+                rect += "\n"
         return rect
         
