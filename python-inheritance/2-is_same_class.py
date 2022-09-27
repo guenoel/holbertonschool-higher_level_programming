0e6b6c50de4fdfe8d3f813c83f612d04b3acf30c
guenoel <guenooel@hotmail.com>
2022-09-27 18:10:35 +0200

    task 2 inheritance Python

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module - no comments"""
 
+
 def is_same_class(obj, a_class):
     """class qui verifie si c'est exactement de cette classe"""
     return type(obj) is a_class
