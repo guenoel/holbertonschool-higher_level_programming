17f3a543649c6bc57f1d40e42cb02bc12f779dc9
guenoel <guenooel@hotmail.com>
2022-09-27 20:35:10 +0200

    task 4 inheritance Python

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """fonction qui verifie si l'objet est une instance 
+    """fonction qui verifie si l'objet est une instance
     d une classe qui est heritée de la classe donnée"""
 
     return isinstance(obj, a_class) and type(obj) != a_class
