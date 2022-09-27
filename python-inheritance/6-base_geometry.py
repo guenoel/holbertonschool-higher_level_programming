81b58d17e069466fe330a76238282425743d4c46
guenoel <guenooel@hotmail.com>
2022-09-27 21:33:27 +0200

    task 6 inheritance Python

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module - no comments"""
+
+
+class BaseGeometry:
+    """classe basegeometry"""
+
+    def area(self):
+        """commentaire de méthode qui leve une exception"""
+
+        raise Exception("area() is not implemented")
