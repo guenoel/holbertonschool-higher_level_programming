bfceec8b29297a78a614c4cbcb1a8b8b20b79150
guenoel <guenooel@hotmail.com>
2022-09-27 18:16:29 +0200

    task 3 inheritance Python

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""module - no comments"""
+
+
 def is_kind_of_class(obj, a_class):
+    """methode qui revoie true si ca fait partie d'un classe parente"""
+    
     return isinstance(obj, a_class)
