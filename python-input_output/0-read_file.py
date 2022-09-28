95df775b6a06631fa687ba5f4592d99c6d02c21f
guenoel <guenooel@hotmail.com>
2022-09-28 11:58:07 +0200

    task 0 I/O Python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """Commentaire de méthode"""
 
     with open(filename, mode="r", encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end='')
     a_file.closed
