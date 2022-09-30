15adb334d105a4187eb53401339e10fa3357e002
guenoel <guenooel@hotmail.com>
2022-09-30 15:18:31 +0200

    Task 10 I/O Python

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" My module comment
+"""
+
+
+from pickle import APPEND
+from readline import append_history_file
+
+
+class Student:
+    """ My class student
+    """
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_dict = dict()
+
+        if type(attrs) is list and all(isinstance(ele, str) for ele in attrs):
+            for ele in attrs:
+                if ele in self.__dict__:
+                    my_dict.update({ele: self.__dict__[ele]})
+            return my_dict
+        else:
+            return self.__dict__
