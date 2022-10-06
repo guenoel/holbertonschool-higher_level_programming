3849332a4b2e076746cc1e0736a7ffd1f9e2ba17
guenoel <guenooel@hotmail.com>
2022-10-06 14:52:00 +0200

    Task 10 almost_a_circle Python

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Commentaire de module"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    """class for square that inherits from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format
+                (self.id, self.x, self.y, self.width))
