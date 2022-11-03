82fb3453035b93288b5b5fd2a226a6b0118d5f02
guenoel <guenooel@hotmail.com>
2022-11-03 06:31:08 -0700

    create table full

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- full creation
+INSERT INTO hbtn_0c_0.second_table (
+    id,
+    name,
+    score
+)
+VALUES
+    (
+        1,2,3,4
+    ),
+    (
+        'John','Alex','Bob','George'
+    ),
+    (
+        10,3,14,8
+    );
