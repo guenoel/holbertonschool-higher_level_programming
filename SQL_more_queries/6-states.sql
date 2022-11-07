1dd46b761ff77dc64d2a9d1a461de082980acd57
guenoel <guenooel@hotmail.com>
2022-11-07 07:46:15 -0800

    create table with unique auto generated values SQL

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create column with unique values
-CREATE DATABASE IF NOT EXISTS states;
-CREATE TABLE IF NOT EXISTS unique_id (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
     id INT DEFAULT 1 PRIMARY KEY,
     name VARCHAR(256)
 )
