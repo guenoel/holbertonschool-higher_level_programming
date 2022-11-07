aa08c985801c85859e8b857b90f0b5b6bab584a5
guenoel <guenooel@hotmail.com>
2022-11-07 11:05:34 -0800

    create table with foreign key SQL

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
     state_id INT,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
 )
