b601234b275582643a56207d4b12c8926e8ad23f
guenoel <guenooel@hotmail.com>
2022-11-07 14:07:50 -0800

    select multiple table outer join ordered

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- select columns from different tables
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY title ASC, genre_id ASC;
