79a6a5b1de016fa0a7ea5b78ee40f8fdb6f77b95
guenoel <guenooel@hotmail.com>
2022-11-08 06:09:42 -0800

    print query from Python

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Connect to a MySQL database with MySQLdb
+connector and print result of query"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id""")
+    for row in c.fetchall():
+        print(row)
