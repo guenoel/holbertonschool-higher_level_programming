6860477611c267f597a42cf7659592ca55046cb1
guenoel <guenooel@hotmail.com>
2022-11-08 05:58:37 -0800

    print query from Python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,9 @@
 from sys import argv
 import MySQLdb
 
-db=MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],db=argv[3])
-c=db.cursor()
-c.execute("""SELECT * FROM states""")
-for row in c.fetchall():
-    print(row)
+if __name__ == "__main__":
+    db=MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],db=argv[3])
+    c=db.cursor()
+    c.execute("""SELECT * FROM states""")
+    for row in c.fetchall():
+        print(row)
