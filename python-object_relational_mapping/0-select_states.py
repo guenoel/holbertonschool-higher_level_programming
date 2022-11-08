dfd597527e638dc3a8ad4d85a5e7c59a112fce98
guenoel <guenooel@hotmail.com>
2022-11-08 06:02:20 -0800

    print query from Python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Connect to a MySQL database with MySQLdb 
+"""Connect to a MySQL database with MySQLdb
 connector and print result of query"""
 
 
@@ -7,7 +7,12 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],db=argv[3])
+    db = MySQLdb.connect(
+    host="localhost",
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3]
+    )
     c = db.cursor()
     c.execute("""SELECT * FROM states""")
     for row in c.fetchall():
