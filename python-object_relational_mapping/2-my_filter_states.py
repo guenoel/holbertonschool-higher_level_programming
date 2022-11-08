ee18dee1d242bcec3789ea6ae495f54af083dd1d
guenoel <guenooel@hotmail.com>
2022-11-08 06:47:02 -0800

    print query from Python

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Connect to a MySQL database with MySQLdb
+connector and print result of query"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id""")
+    for row in c.fetchall():
+        if row[1] == argv[4]:
+            print(row)
