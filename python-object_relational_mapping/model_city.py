8ef4677e0c0c5514881c4fd57454259adeb04df5
guenoel <guenooel@hotmail.com>
2022-11-09 15:22:48 -0800

    engine + session + join

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,10 +7,12 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
+
 class City(Base):
     """city class: id + name"""
 
     __tablename__ = 'cities'
     id = Column("id", Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column("state_id", Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column("state_id", Integer,
+     ForeignKey(State.id), nullable=False)
