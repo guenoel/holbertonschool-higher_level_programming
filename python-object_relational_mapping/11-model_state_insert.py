779c0c907f7b5d22e988ccf68733f2ef2af4ca74
guenoel <guenooel@hotmail.com>
2022-11-09 13:04:30 -0800

    engine + session + add row

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""List all state objects"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == "__main__":
+    Base = declarative_base()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    result = session.query(State).filter_by(name='Louisiana').first()
+
+    if result is not None or result:
+        print(result.id)
+    else:
+        print("Not found")
