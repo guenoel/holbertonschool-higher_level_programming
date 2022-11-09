cf256f5c9962b7c6566cf0f8a4d28e59aa4d5cc0
guenoel <guenooel@hotmail.com>
2022-11-09 15:53:42 -0800

    delete contains

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,9 +11,13 @@ from sqlalchemy import delete
 if __name__ == "__main__":
     Base = declarative_base()
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter(State.name.contains('a')).delete(synchronize_session='fetch')
+    session.query(State).filter(State.name.contains('a'))\
+        .delete(synchronize_session='fetch')
     session.commit()
