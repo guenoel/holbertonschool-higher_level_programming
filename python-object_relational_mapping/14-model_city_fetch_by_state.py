16059fdfc800afd27bd4da8d079a5e7004d4d231
guenoel <guenooel@hotmail.com>
2022-11-09 15:29:14 -0800

    engine + session + join

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     Base = declarative_base()
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1],
-                           sys.argv[2],
-                           sys.argv[3]),
+                                   sys.argv[2],
+                                   sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     result = session.query(State.name, City.id, City.name)\
-                           .filter(State.id == City.state_id)
+             .filter(State.id == City.state_id)
 
     for row in result:
         print("{}: ({}) {}".format(row[0], row.id, row.name))
