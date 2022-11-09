f3a7b9600f4e37fe3a1130d12601f6d1af71ef91
guenoel <guenooel@hotmail.com>
2022-11-09 07:39:24 -0800

    SQL_Alchemy first task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,5 +14,5 @@ class State(Base):
     """state class: id + name"""
 
     __tablename__ = 'states'
-    id=Column("id", Integer, primary_key=True)
-    name=Column(String(128), nullable=False)
+    id = Column("id", Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
