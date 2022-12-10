319c8cad63d3f7a7f3c47c4c9fc73b4338f67652
guenoel <guenooel@hotmail.com>
2022-12-10 07:16:02 -0800

    task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+square
+*/
+
+const arg = process.argv[2];
+if (arg === undefined || isNaN(arg)) {
+  console.log('Missing size')
+}
+for (let i = 0; i < arg; i++) {
+  for (let i = 0; i < arg; i++) {
+    process.stdout.write('#');
+  }
+  process.stdout.write('\n');
+}
