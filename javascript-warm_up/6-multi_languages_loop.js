f04122bd9d42dd63ebf2de71b60cc8951b1035f8
guenoel <guenooel@hotmail.com>
2022-12-10 03:38:31 -0800

    task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+loop
+*/
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; languages[i] != undefined; i++) {
+  console.log(languages[i])
+}
