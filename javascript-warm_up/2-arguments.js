523ca1ded554a6397cadb6bca11fe6ff6a323090
guenoel <guenooel@hotmail.com>
2022-12-10 02:19:39 -0800

    task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+count arguments
+*/
+
+if (process.argv.length < 3) {
+    console.log('No argument');
+  } else if (process.argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
