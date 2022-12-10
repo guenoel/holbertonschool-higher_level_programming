4264759afc9b83c2b04033d0f504f55933e82b68
guenoel <guenooel@hotmail.com>
2022-12-10 13:55:22 -0800

    task 3 + modif task 2

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print () {
+      for (let i = 0; i < this.height; i++) {
+          for (let i = 0; i < this.width; i++) {
+            process.stdout.write('X');
+          }
+          process.stdout.write('\n');
+        }
+    }
+  }
+  module.exports = Rectangle;
+  
