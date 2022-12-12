bec69de6fe8747fe495193f028c11a052d06fd65
guenoel <guenooel@hotmail.com>
2022-12-12 05:16:34 -0800

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-fs.readFile (process.argv[2], function (err, toto) {
+const fs = require('fs');
+fs.readFile(process.argv[2], function (err, toto) {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-      console.log(toto.toString());
-})
+  console.log(toto.toString());
+});
