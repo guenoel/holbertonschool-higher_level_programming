3431aec4142b2bf41cbc4068607021774bdad9ae
guenoel <guenooel@hotmail.com>
2022-12-12 14:24:42 -0800

    advanced 1

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -6,12 +6,13 @@ request(theURL, function (error, response) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
   }
-  listURL = JSON.parse(response.body).characters
+  listURL = JSON.parse(response.body).characters;
   for (URLchar of listURL) {
     request(URLchar, function (error, response) {
-        if (error) {
-          console.error('error:', error); // Print the error if one occurred
-        }
-        console.log(JSON.parse(response.body).name);
-  })
-}});
+      if (error) {
+        console.error('error:', error); // Print the error if one occurred
+      }
+      console.log(JSON.parse(response.body).name);
+    });
+  }
+});
