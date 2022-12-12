5a4f50c468ec887429fe2cec19a4490378129b39
guenoel <guenooel@hotmail.com>
2022-12-12 14:41:15 -0800

    advanced 2

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
+function compareNumbers(a, b) {
+    return a - b;
+  }
+
 const theURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(theURL, function (error, response) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
   }
-  const listURL = JSON.parse(response.body).characters.sort();
+  const listURL = JSON.parse(response.body).characters.sort(compareNumbers);
   for (const URLchar of listURL) {
     request(URLchar, function (error, response) {
       if (error) {
