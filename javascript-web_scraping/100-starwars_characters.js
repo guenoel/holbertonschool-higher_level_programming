86e1d884e8d5e75decc812d7263ec7cecb0e4aa3
guenoel <guenooel@hotmail.com>
2022-12-12 14:24:03 -0800

    advanced 1

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+
+const theURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(theURL, function (error, response) {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  listURL = JSON.parse(response.body).characters
+  for (URLchar of listURL) {
+    request(URLchar, function (error, response) {
+        if (error) {
+          console.error('error:', error); // Print the error if one occurred
+        }
+        console.log(JSON.parse(response.body).name);
+  })
+}});
