4767e93cc9928c0a37cf5ea2d72fede4aa75afc2
guenoel <guenooel@hotmail.com>
2022-12-13 12:08:14 -0800

    fix semistandard sauf $

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-
 $.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   const res = data.results;
-  for (film of res) {
-  $('#list_movies').append('<li>' + film.title + '</li>');
-}
+  for (const film of res) {
+    $('#list_movies').append('<li>' + film.title + '</li>');
+  }
 });
