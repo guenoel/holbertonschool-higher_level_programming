128bd4528dca43ec357bc3c0596633cdb8f25d72
guenoel <guenooel@hotmail.com>
2022-12-15 07:52:11 -0800

    task 2 advanced

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -3,12 +3,12 @@ const request = require('request');
 
 const theURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(theURL, async function (error, response) {
-  if (error) {console.error('error:', error);}
+  if (error) { console.error('error:', error); }
   const listURL = JSON.parse(response.body).characters;
   for (const URLchar of listURL) {
     await new Promise((resolve, reject) => {
       request(URLchar, function (error, response) {
-        if (error) { reject(err); }
+        if (error) { reject(error); }
         console.log(JSON.parse(response.body).name);
         resolve();
       });
